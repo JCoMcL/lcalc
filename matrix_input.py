a23e8bb22e8c042699eb4c94b240ede041579530
JCoMcL <rapcyss@gmail>
2020-05-06 16:27:56 +0100

    use curses implementation of readchar (removing exteranl dependancy), also clear curses screen

--- matrix_input.py
+++ matrix_input.py
@@ -1,17 +1,16 @@
 #!/bin/env python3
 
-from readchar import readchar
 import sys
 import curses
 from numpy import matrix
-
 
 def startcurses():
     stdscr = curses.initscr()
     curses.noecho()
     curses.cbreak()
     return stdscr
-def endcurses():
+def endcurses(stdscr):
+    stdscr.clear()
     curses.nocbreak()
     curses.echo()
     curses.endwin()
@@ -30,16 +29,16 @@ def read_matrix(a = None):
         a = [[""]]
     stdscr = startcurses()
     while True:
-        c = str(readchar())
+        c = stdscr.getkey()
         if c == ' ' and (len(a) == 1 or len(a[-1]) < len(a[0])):
             a[-1][-1] = string2num(a[-1][-1])
             a[-1].append("")
         
-        elif c == '\r' or c == ' ': #return
+        elif c == '\n' or c == ' ': #return
             #exit function by pressing return twice
             if not a[-1][0]:
                 a.pop()
-                endcurses()
+                endcurses(stdscr)
                 return matrix(a)
 
             a[-1][-1] = string2num(a[-1][-1])
@@ -76,7 +75,6 @@ def show_matrix(mat, stdscr):
     cw = col_widths(smat)
     for i in range(len(smat)):
         stdscr.addstr(i, 0, " ".join( [smat[i][j].ljust(cw[j]) for j in range(len(smat[i]))]))
-    stdscr.refresh()
 
 if __name__ == "__main__":
     print(read_matrix())
